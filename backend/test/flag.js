e761d446a4a5630b9c5d8b5c00aa7f4c2e985c61
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 18:39:16 +0300

    edit test

--- backend/test/flag.js
+++ backend/test/flag.js
@@ -1,32 +1,8 @@
-const request = require('request');
-const base_url = "http://localhost:3000/";
+const app = express();
 
-describe("employee can flag gifs, comments and articles as inappropriate", () => {
-    describe("GET /", () => {
-        it("return a response status code of 200", () => {
-            request.get(base_url,(error,response,body) =>{
-                expect(response.statusCode).toBe(200);
-                done();
-            });
-        });
-
-        it("send message body with gif title", () => {
-            request.post(base_url, (error,request,body) => {
-                expect(body).toContain(articleTitle);                
-                done();
-            });
-        });
-
-        it("send message body with article title", () => {
-            request.post(base_url, (error, request,body) => {                
-                expect(body).toContain(articleTitle);
-            });
-        });
-
-        it("send message body with comment title", () => {
-            request.post(base_url, (error, request,body) => {
-                expect(body).toContain(comment);
-            });
-        });
-    });
+app.get("/", (req,res) => {
+    req.params.articleTitle;          
+    req.params.gifArticle;
+    req.params.comment;    
+    app.listen(3000);
 });
