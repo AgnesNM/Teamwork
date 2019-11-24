1ee86ec925e75274c612772623226c50879a7f93
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 17:55:59 +0300

    view spec article test

---
+++ backend/spec/view_spec_article_spec.js
@@ -0,0 +1,22 @@
+const base_url = "http://localhost:3000/";
+
+
+describe("employee can view a specific article", () => {
+    describe("GET /", () => {
+        it("return a response status code of 200", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe(200);
+            });
+        });
+
+        it("send message body with article title", () => {
+            request.post(base_url, (error,request,body) => {
+                expect(body).toContain(articleTitle);
+                done();
+            });
+        });
+    });
+});        
+        
+      
+
