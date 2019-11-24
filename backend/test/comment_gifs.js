bb5dfbfd2088b15e2f0acb2375fa17fce931dc34
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 17:43:04 +0300

    comment gif test

---
+++ backend/test/comment_gifs.js
@@ -0,0 +1,25 @@
+const base_url = "http://localhost:3000/";
+
+describe("employee can comment on gifs", () => {
+    describe("GET /", () => {
+        it("return a response status code of 200", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });        
+
+        it("return a successful message body", () => {
+            request.get(base_url, (error, response, body) => {                
+                expect(body).toBe('comment updated!');
+                done();
+            });
+        });
+ 
+        it("send message body with comment", () => {
+            request.post(base_url, (errempor, request,body) => {
+                expect(body).toContain(comment);
+            });
+        });
+    });
+});
