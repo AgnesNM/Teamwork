97bae3cea1a4c497abd0c1867d3ef3ffcbd49f00
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 20:12:08 +0300

    working endpoint

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -30,11 +30,11 @@ router.post('/', async (req, res, next) => {
   ];
   const text = 'INSERT INTO employees(first_name,last_name,email,password, gender, job_role, department, address, emp_id, db_role) VALUES($1,$2,$3,$4,$5, $6, $7, $8, $9, $10) RETURNING *';
   
-  const {rows} = db.query(text,values, (err,res) => {
-    if(err){   
-        console.log(err.stack);  
+  const {rows} = await db.query(text,values, (err,res) => {
+    if(err){           
+        return res.status(400).send(error.stack); 
     }  else {
-        console.log(res.rows[0]);         
+      return res.status(201).send(rows[0]);        
       }        
     });
 });
