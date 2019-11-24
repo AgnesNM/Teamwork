7df54c1bd64ce81a9e8ca655e7c65cc673adbc95
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 18:31:21 +0300

    delete flagged items test

---
+++ backend/test/del_flag.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res) => {
+app.delete("/", (req,res) => {     
+    req.params.articleTitle,
+    req.params.gifTitle,
+    req.params.comment,    
+    app.listen(3000);
+});
+});
