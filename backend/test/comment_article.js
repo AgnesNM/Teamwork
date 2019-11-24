bf12a36e187edc8eed99e4af93049ebbd3cc6de9
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 17:36:27 +0300

    comment article test

---
+++ backend/test/comment_article.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res) => {
+app.post("/", (req,res) => {
+    req.params.articleTitle;    
+    req.params.comments;    
+    app.listen(3000);
+});
+});
