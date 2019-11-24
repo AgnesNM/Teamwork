ddaf6aaab6dd3d2a7971c6fafc041050d94f9542
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 17:21:06 +0300

    edit test

--- backend/spec/del_article_spec.js
+++ backend/spec/del_article_spec.js
@@ -1,11 +1,13 @@
+const request = require("request");
 const base_url = "http://localhost:3000/";
-
 
 
 describe("employee can delete their articles", () => {
     describe("DELETE /", () => {
         it("return a response status code of 200", () => {
             request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe(200);
+                done();
             });
         });
         it("return a successful message body", () => {
