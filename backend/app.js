afcd9c3dabfa100472bf8c37c0aad3bdb109e8a7
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-24 13:39:54 +0300

    add dependencies

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+/* eslint-disable object-shorthand */
+const { Pool } = require('pg');
+
+const connectionString = 'postgres://nduta:e1234f@localhost:5432/Teamwork';
+
+const pool = new Pool({
+  connectionString: connectionString,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  // eslint-disable-next-line no-console
+  console.log(err, res);
+  pool.end();
+});
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+module.exports = app;
