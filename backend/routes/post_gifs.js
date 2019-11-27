c2b44fe3b2b1553f40c8e5e36b69cae93992f867
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-27 08:45:45 +0300

    import cloudinary

--- backend/routes/post_gifs.js
+++ backend/routes/post_gifs.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 
 const bodyParser = require('body-parser');
+var cloudinary = require('cloudinary').v2;
 
 const connectionString = 'postgres://nduta:e1234f@localhost:5432/Teamwork';
 
@@ -20,9 +21,10 @@ router.post('/', (req, res, next) => {
     req.body.gif_title,
     req.body.gif_author,
     req.body.date_created,
-    req.body.category
+    req.body.category,
+    req.body.gif_URL
     ];
-    const text = 'INSERT INTO gifs(gif_id,gif_title,gif_author,date_created, category) VALUES($1,$2,$3,$4,$5) RETURNING *';
+    const text = 'INSERT INTO gifs(gif_id,gif_title,gif_author,date_created, category, gif_URL) VALUES($1,$2,$3,$4,$5, $6) RETURNING *';
     pool.query(text,values, (err,res) => {
         if(err){ 
             console.log(err.stack); 
