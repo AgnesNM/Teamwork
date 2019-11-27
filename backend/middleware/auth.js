99d61c70de88f3483fef905a158efc6c08ad67c4
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-27 04:19:01 +0300

    trying authentication out

---
+++ backend/middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const { Pool } = require('pg');
+
+const bodyParser = require('body-parser');
+
+const connectionString = 'postgres://nduta:e1234f@localhost:5432/Teamwork';
+
+const pool = new Pool({
+connectionString: connectionString,
+});
+
+
+module.exports = (req,res,next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const employee = decodedToken.password;
+            if (req.body.password !== public.password){
+                throw 'you are not allowed to access this protected route!';
+            } else {
+                next();
+            }
+            } catch {
+                res.status(401).json({
+                error: new Error('Invalid request!')
+                });
+            }
+};
+
