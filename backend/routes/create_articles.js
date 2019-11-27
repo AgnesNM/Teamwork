1c45e27f98ba556dc7ab5809e58df88eb000c6fb
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-27 07:26:59 +0300

    implementing authentication

--- backend/routes/create_articles.js
+++ backend/routes/create_articles.js
@@ -14,16 +14,16 @@ const pool = new Pool({
 connectionString: connectionString,
 });
 
-router.post('/', (req, res, next) => { 
-const values = [
-req.body.article_id,
-req.body.article_title,
-req.body.article_author,
-req.body.date_created,
-req.body.category
-];
-const text = 'INSERT INTO articles(article_id,article_title,article_author,date_created, category) VALUES($1,$2,$3,$4,$5) RETURNING *';
-pool.query(text,values, (err,res) => {
+router.post('/', async (req, res, next) => { 
+    const values = [
+    req.body.article_id,
+    req.body.article_title,
+    req.body.article_author,
+    req.body.date_created,
+    req.body.category
+    ];
+    const text = 'INSERT INTO articles(article_id,article_title,article_author,date_created, category) VALUES($1,$2,$3,$4,$5) RETURNING *';
+const {rows} = await db.query(text,values, (err,res) => {
 if(err){ 
 console.log(err.stack); 
 } else {
